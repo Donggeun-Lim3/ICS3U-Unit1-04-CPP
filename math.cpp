fa97c8b6616625603e219209392f7ff450580dba
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-20 20:56:31 +0000

    math

---
+++ math.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved
+//
+// Created by Donggeun Lim
+// Created on November 2020
+// This is math program
+
+
+#include <iostream>
+
+int main()  {
+    std::cout << "If a rectangle has the dimension:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "Area is " << (5*3) << "cm^2" << std::endl;
+    std::cout << "perimeter is " << (2*(5+3)) << "cm" << std::endl;
+}
